01391c150bc4513fd2ba10b7fde19055706d6b21
Harsh <95859326+harsh5ingh@users.noreply.github.com>
2025-08-17 20:38:03 +0000

    variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 5216
+let accountEmail = "harshkr.5216@gmail.com"
+var accountPassword = "harsh1234"
+accountCity = "Patna"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
