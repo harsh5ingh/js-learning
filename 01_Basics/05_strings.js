8b57606760f2ac47de649f465ae02f8498a22d44
Harsh <95859326+harsh5ingh@users.noreply.github.com>
2025-08-22 10:15:41 +0000

    strings

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,47 @@
+const name = "harsh-hs"
+const repoCount = 50
+
+//console.log(name + repoCount + "Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('harshhs')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt('4'));
+console.log(gameName.indexOf('s'));
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherString = gameName.slice(-3,4)
+console.log(anotherString);
+
+const  newStringOne = "    harsh    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://harsh.com/harsh%20singh"
+
+console.log
+(url.replace('%20', '-'))
+
+console.log
+(url.includes('harsh'))
+
+console.log(gameName.split('-'));
+
+
+
+
+
+
+
+
+
+
