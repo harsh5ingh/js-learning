144d0a7d1c6f191a41ae20449a664312dba9eb81
Harsh <harrsh0502@gmail.com>
2025-11-22 16:49:07 +0530

    fxn

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,32 @@
+
+function sayMyName(){
+console.log("H");
+console.log("A");
+console.log("R");
+console.log("S");
+console.log("H");
+}
+
+// sayMyName()
+
+/* function addTwoNumbers(number1, number2){ 
+  // here number1 & number2 are paramters
+  console.log(number1 + number2);
+  
+} */
+
+
+  function addTwoNumbers(number1, number2){ 
+
+    //let result = number1 + number2
+    // console.log("harsh");
+    //return result // result k baad kuchh bhi print ni hota h
+    return number1 + number2
+    
+  
+}
+const result = addTwoNumbers(2, 5) 
+
+//console.log("Result", result);
+
+ 
