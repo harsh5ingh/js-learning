3a09e2c123855a6f4df0a28771b843e6bc609e87
Harsh <harrsh0502@gmail.com>
2026-06-01 22:33:26 +0530

    learned about reduce.

--- 05_iterations/reduce.js
+++ 05_iterations/reduce.js
@@ -7,3 +7,26 @@ const myTotal = myNums.reduce(function (acc, currval){
 }, 0)
 
 console.log(myTotal);
+
+
+const shoppingCart = [
+  {
+    itemName: "Js course",
+    price: 2999
+  },
+  {
+    itemName: "Python course",
+    price: 999
+  },
+  {
+    itemName: "DSA course",
+    price: 6999
+  },
+  {
+    itemName: "Mobile Dev course",
+    price: 4899
+  }
+]
+
+const PriceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+console.log(PriceToPay);
